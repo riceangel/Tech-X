dabec11c68d05d76923e05a177faeec6625fd0f0
riceangel <43398049+riceangel@users.noreply.github.com>
2021-07-19 15:53:57 -0700

    Working buttons to update "calendar" screen :D

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//File currently not in use, couldn't figure out why using buttons to update calendar wasn't working
 var jan = document.getElementById("Jan");
 var title = document.getElementById("nav-title");
 var monthhbar = document.getElementById("monthbar");
@@ -8,5 +9,5 @@ document.getElementById("Jan").addEventListener("click", updateJan());
 
 
 function updateJan() {
-    title.innerHTML = "potato";
+    col1.innerHTML = "potato";
 }
