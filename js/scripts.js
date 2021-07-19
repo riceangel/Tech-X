a24137377c3fa86df16a12c86752ee36f277d976
riceangel <43398049+riceangel@users.noreply.github.com>
2021-07-19 14:38:22 -0700

    Revised navbar to go to Amazon.com | attempting to fix js (WIP)

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var jan = document.getElementById("Jan");
+var title = document.getElementById("nav-title");
+var monthhbar = document.getElementById("monthbar");
+var col1 = document.getElementById("col1");
+
+document.getElementById("Jan").addEventListener("click", january());
+
+
+
+function january() {
+    document.getElementById("col1").innerHTML = "potato";
+}
